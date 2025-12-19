0939fc3b3f5447bd9b53a2a476b62f82caa9c6e3
Youcef <youcefcopy@gmail.com>
2025-12-19 22:17:50 +0100

    setting up the necessary function of the app

--- js/app.js
+++ js/app.js
@@ -3,3 +3,32 @@ console.log("js has been loaded!");
 // using event delegation for the buttons
 const btnContainer = document.querySelector('.media-btns');
 console.log(btnContainer);
+
+
+function playMusic() {
+    
+}
+
+function stopMusic() {
+    
+}
+
+function pauseMusic() {
+    
+}
+
+function moveForward() {
+    
+}
+
+function moveBackward() {
+    
+}
+
+function nextMusic() {
+    
+}
+
+function previousMusic() {
+    
+}
