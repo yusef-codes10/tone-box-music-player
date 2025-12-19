0a6650972bbc140029adc182f8715002c4254625
Youcef <youcefcopy@gmail.com>
2025-12-19 22:39:04 +0100

    btns works in one event listener

--- js/app.js
+++ js/app.js
@@ -7,6 +7,16 @@ console.log(btnContainer);
 btnContainer.addEventListener('click', e => {
     if (e.target.classList.contains('btn')) {
         console.log('yes, that\'s your target!');
+    }     if (e.target.classList.contains('backward-step-btn')) {
+        console.log('yes, that\'s backward!');
+    }    if (e.target.classList.contains('backward-btn')) {
+        console.log('yes, that\'s btn backward-btn!');
+    }    if (e.target.classList.contains('play-btn')) {
+        console.log('yes, that\'s play-btn!');
+    }    if (e.target.classList.contains('forward-btn')) {
+        console.log('yes, that\'s forward-btn!');
+    }    if (e.target.classList.contains('forward-step-btn')) {
+        console.log('yes, that\'s forward-step-btn!');
     }
 })
 
