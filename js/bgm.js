3a85c593901ba32d8ac6dfb28bc8840396f943f3
Youcef <youcefcopy@gmail.com>
2025-12-20 19:48:26 +0100

    1st obj has been created

--- js/bgm.js
+++ js/bgm.js
@@ -1,1 +1,15 @@
 // here, we are to create an array of music objects {id, image, audio}
+export const bgmList = [
+    {
+        id: 'armor-hero-main',
+        title: 'Armor Hero Main',
+        img: '',
+        ost: new Audio('/assets/audio/armor-hero-main.mp3'),
+    },
+    {
+
+    },
+
+
+
+];
