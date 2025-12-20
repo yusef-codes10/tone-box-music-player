b48f67c94eb518e83f79ba95fe868f1a305eeb79
Youcef <youcefcopy@gmail.com>
2025-12-20 19:49:40 +0100

    we will have two obj for now

--- js/bgm.js
+++ js/bgm.js
@@ -7,9 +7,9 @@ export const bgmList = [
         ost: new Audio('/assets/audio/armor-hero-main.mp3'),
     },
     {
-
-    },
-
-
-
+        id: 'burning-town',
+        title: 'Burning Town',
+        img: '',
+        ost: new Audio('/assets/audio/burning-town.mp3'),
+    }
 ];
