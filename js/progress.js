0984176f1b890aa33a5f9bcb26947aacdaf25e93
Youcef <youcefcopy@gmail.com>
2025-12-23 21:41:45 +0100

    changing the width every seconde

--- js/progress.js
+++ js/progress.js
@@ -5,3 +5,9 @@ const bar = document.querySelector('.bar');
 let progress = 0;
 
 // each time the music passes we increemnt
+
+const interval = setInterval(() => {
+    progress += 10;
+
+    bar.computedStyleMap.width = progress + '%';
+}, 1000);
