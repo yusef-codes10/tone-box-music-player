77541cc4ab7cc39c52c8e3eee429bf7b3b6f4a15
Youcef <youcefcopy@gmail.com>
2025-12-23 21:08:24 +0100

    the volumeUp() method

--- js/app.js
+++ js/app.js
@@ -108,7 +108,8 @@ function previousMusic() {
 }
 
 function volumeUp() {
-
+    const audio = bgmList[currentMusicId].ost;
+    audio.volume = Math.min(1, audio.volume + step);
 }
 
 function volumeDown() {
