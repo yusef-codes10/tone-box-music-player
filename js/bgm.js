ec47edc2889c4f8249c11b80e4efa4c680f926d9
Youcef <youcefcopy@gmail.com>
2025-12-24 20:31:25 +0100

    the img path

--- js/bgm.js
+++ js/bgm.js
@@ -3,19 +3,19 @@ export const bgmList = [
     {
         id: 'armor-hero-main',
         title: 'Armor Hero Main',
-        img: '',
+        img: '/assets/images/1.jpeg',
         ost: new Audio('/assets/audio/armor-hero-main.mp3'),
     },
     {
         id: 'burning-town',
         title: 'Burning Town',
-        img: '',
+        img: '/assets/images/2.jpeg',
         ost: new Audio('/assets/audio/burning-town.mp3'),
     }, 
     {
         id: 'fight-theme',
         title: 'Fight Theme',
-        img: '',
+        img: '/assets/images/3.jpeg',
         ost: new Audio('/assets/audio/fight-theme.mp3'),
     }
 ];
