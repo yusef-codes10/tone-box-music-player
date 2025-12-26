0f96f24184b2865368f2efaa3452c5cf3205109e
Youcef <youcefcopy@gmail.com>
2025-12-26 21:22:58 +0100

    export in  one line

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,25 +1,26 @@
 // This file is for the progress bar logic
-export const progressTime = document.querySelector('.progress-time');
+const progressTime = document.querySelector('.progress-time');
 
 // the progress bar functionality
-export const bar = document.querySelector('.bar');
+const bar = document.querySelector('.bar');
 
 
 let musicDuration = 0;
-export function setMusicDuration(id) {
+function setMusicDuration(id) {
     musicDuration = id;
 }
 
-export function getMusicDuraion() {
+function getMusicDuraion() {
     return musicDuration;
 }
 
 let currentMusicTime = 0;
-export function setCurrentMusicTime(id) {
+function setCurrentMusicTime(id) {
     currentMusicTime = id;
 }
 
-export function getCurrentMusicTime() {
+function getCurrentMusicTime() {
     return currentMusicTime;
 }
 
+export {progressTime, bar, setCurrentMusicTime, getCurrentMusicTime, setMusicDuration, getMusicDuraion};
